c56de9e126cf15984fef89f181da776b4800c145
MrLipa2496 <sasalipa216@gmail.com>
2024-11-23 16:21:38 +0200

    Refactor migrate style

---
+++ server/src/migrations/20241123134929-create-Transactions.js
@@ -0,0 +1,51 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up (queryInterface, Sequelize) {
+    return queryInterface
+      .createTable('Transactions', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        amount: {
+          allowNull: false,
+          type: Sequelize.DECIMAL,
+        },
+        operationType: {
+          allowNull: false,
+          type: Sequelize.ENUM(['INCOME', 'EXPENSE']),
+        },
+        userId: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+          references: {
+            model: 'Users',
+            key: 'id',
+          },
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE',
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      })
+      .then(() =>
+        queryInterface.addConstraint('Transactions', {
+          fields: ['amount'],
+          type: 'check',
+          where: { amount: { [Sequelize.Op.gt]: 0 } },
+        })
+      );
+  },
+  down (queryInterface, Sequelize) {
+    return queryInterface.dropTable('Transactions');
+  },
+};

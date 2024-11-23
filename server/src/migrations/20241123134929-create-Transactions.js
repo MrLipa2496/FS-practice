db0a1e6903c69603d721a83079efc32c282e547b
MrLipa2496 <sasalipa216@gmail.com>
2024-11-23 16:24:06 +0200

    Refactor migration func to arrow

--- server/src/migrations/20241123134929-create-Transactions.js
+++ server/src/migrations/20241123134929-create-Transactions.js
@@ -1,7 +1,7 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  up (queryInterface, Sequelize) {
+  up: (queryInterface, Sequelize) => {
     return queryInterface
       .createTable('Transactions', {
         id: {
@@ -45,7 +45,7 @@ module.exports = {
         })
       );
   },
-  down (queryInterface, Sequelize) {
+  down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('Transactions');
   },
 };
